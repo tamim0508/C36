6edf6af22d5980d0807e06ddc4c4005d124cc35f
Altaf Sarker <ahsarker@AHSarker-M07.local>
2020-06-17 18:47:13 -0700

    game

--- js/Game.js
+++ js/Game.js
@@ -23,4 +23,30 @@ class Game {
       form.display();
     }
   }
+  play(){
+    form.hideFunction();
+    textSize(30);
+    text("Game Start", 120, 300);
+    Player.getPlayerInfo();
+
+    if(allPlayers !== undefined){
+      var displayposition = 130;
+      for(var plr in allPlayers){
+        if(plr === "player" + player.index){
+          fill("red");
+          
+        }
+        else{
+          fill("blue");
+        }
+        displayposition = displayposition+20;
+        textSize(15)
+        text(allPlayers[plr].name + ":"+allPlayers[plr].distance, 120,displayposition);
+      }
+    }
+    if(keyIsDown(UP_ARROW)&&player.index !== null){
+      player.distance = player.distance + 10;
+      player.update();
+    }
+  }
 }
